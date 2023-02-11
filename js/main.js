2de72718e28202987b5c342f3963d806a41a8e4d
ABSVega <kuro_san@outlook.es>
2023-02-10 23:09:59 -0600

    treceava_version

--- js/main.js
+++ js/main.js
@@ -39,7 +39,7 @@ window.addEventListener('scroll', () => {
     }
 });
 
-
+//modales del apartado kits
 const modalAdd1 = document.querySelector('#modalHome');
 const modalAdd2 = document.querySelector('#modalWork');
 const modalAdd3 = document.querySelector('#modalPlay');
@@ -54,25 +54,59 @@ const closeModal = (modal) => {
 }
 
 modalAdd1.onclick = (event) => {
-    if(event.target === modalAdd1){
+    if (event.target === modalAdd1) {
         closeModal(modalAdd1);
     }
 };
 
 modalAdd2.onclick = (event) => {
-    if(event.target === modalAdd2){
+    if (event.target === modalAdd2) {
         closeModal(modalAdd2);
     }
 };
 
 modalAdd3.onclick = (event) => {
-    if(event.target === modalAdd3){
+    if (event.target === modalAdd3) {
         closeModal(modalAdd3);
     }
 };
 
 modalAdd4.onclick = (event) => {
-    if(event.target === modalAdd4){
+    if (event.target === modalAdd4) {
         closeModal(modalAdd4);
     }
 };
+
+function App() {}
+
+window.onload = function (event) {
+    var app = new App();
+    window.app = app;
+}
+
+App.prototype.processingButton = function (event) {
+    const btn = event.currentTarget;
+    const carruselList = event.currentTarget.parentNode;
+    const track = event.currentTarget.parentNode.querySelector('#track');
+    const carrusel = track.querySelectorAll('.carrusel');
+
+    const carruselWidth = carrusel[0].offsetWidth;
+
+    const trackWidth = track.offsetWidth;
+    const listWidth = carruselList.offsetWidth;
+
+    track.style.left == "" ? leftPosition = track.style.left = 0 : leftPosition = parseFloat(track.style.left.slice(0,-2) * -1);
+    btn.dataset.button == "button-prev" ? prevAction(leftPosition,carruselWidth, track) : nextAction(leftPosition,trackWidth, listWidth, carruselWidth, track);
+}
+
+let prevAction = (leftPosition, carruselWidth, track) => {
+    if (leftPosition > 0) {
+        track.style.left = `${-1 * (leftPosition - carruselWidth)}px`;
+    }
+}
+
+let nextAction = (leftPosition, trackWidth, listWidth, carruselWidth, track) => {
+    if (leftPosition < (trackWidth - listWidth)) {
+        track.style.left = `${-1 * (leftPosition + carruselWidth)}px`;
+    }
+}
